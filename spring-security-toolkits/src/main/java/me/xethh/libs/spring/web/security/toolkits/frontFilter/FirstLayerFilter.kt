abf7eee657adac1f91318fa247c72bc86759a7f3
Xeth Hung <xcwhung@ogcio.gov.hk>
2019-08-19 18:23:08 +0800

    switching to kotlin

---
+++ spring-security-toolkits/src/main/java/me/xethh/libs/spring/web/security/toolkits/frontFilter/FirstLayerFilter.kt
@@ -0,0 +1,19 @@
+package me.xethh.libs.spring.web.security.toolkits.frontFilter
+
+import me.xethh.libs.spring.web.security.toolkits.frontFilter.configurationProperties.FirstFilterProperties
+import me.xethh.libs.toolkits.logging.WithLogger
+import org.slf4j.Logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.filter.GenericFilterBean
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+
+class FirstLayerFilter() : GenericFilterBean(), WithLogger {
+    @Autowired
+    internal var properties: FirstFilterProperties? = null
+
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, filterchain: FilterChain?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
