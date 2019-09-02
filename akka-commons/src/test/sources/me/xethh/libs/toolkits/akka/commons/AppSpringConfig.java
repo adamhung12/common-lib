36bb6b6cc4ef97e5cb549af97557e8cfd365721c
Xeth Hung <xcwhung@ogcio.gov.hk>
2019-09-02 18:26:27 +0800

    add akka commons

---
+++ akka-commons/src/test/sources/me/xethh/libs/toolkits/akka/commons/AppSpringConfig.java
@@ -0,0 +1,14 @@
+package me.xethh.libs.toolkits.akka.commons;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Base configuration for spring
+ */
+@Configuration
+@ComponentScan("me.xethh.libs.toolkits.akka.commons")
+public class AppSpringConfig
+{
+}
